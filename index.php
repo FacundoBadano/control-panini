a5317b585174b6f467f421359fd4946df887d1f4
FacundoBadano <facundobadano6195@gmail.com>
2020-08-17 22:17:23 -0300

    cambios iniciales

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
-<?php
+<!DOCTYPE html>
 
+<?php
+phpinfo();
 ini_set('xdebug.var_display_max_depth', 10);
 ini_set('xdebug.var_display_max_children', 2048);
 ini_set('xdebug.var_display_max_data', 102400);
 ini_set('session.gc_maxlifetime', 86400);
 
-echo "<hr />";
-echo "<h2>Tabla control</h2>";
-echo "<hr />";
+<br>
+<hr />
+<h2>Tabla control</h2>
+<hr />
 
-/*
+
 <div class="row">
 	<div class="col-sm-12">
-		<table class="table table-hover table-condened table-bordered">
+		<table class="table table-hover table-condensed table-bordered">
 			<tr>
 
 			</tr>
 		</table>	
 	</div>
 </div>
-*/
 
-?>
+
+
 
 
