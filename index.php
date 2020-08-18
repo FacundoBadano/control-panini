c06f0a2f498355be43577ae3ffaf01426857180c
FacundoBadano <facundobadano6195@gmail.com>
2020-08-18 20:33:32 -0300

    agrego librerias y comienzo a hacer tabla, buscando un buen formato

--- index.php
+++ index.php
@@ -1,20 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<title>Tabla Figuritas</title>
+	<link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="lib/alertifyjs/css/themes/default.css">
+	<link rel="stylesheet" type="text/css" href="lib/alertifyjs/css/alertify.css">
+	<script src="lib/jquery-3.5.1.min.js"></script>
+	<script src="lib/bootstrap/js/bootstrap.js"></script>
+	<script src="lib/alertifyjs/alertify.js"></script>
 </head>
 <body>
-
+	<h2> JAJAJA</h2>
+	<div class="container">
+		<div id="tabla"></div>
+	</div>
 </body>
 </html>
 
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#tabla').load('tabla.php')
+	});
+</script>
 <?php
 //phpinfo();
 ini_set('xdebug.var_display_max_depth', 10);
 ini_set('xdebug.var_display_max_children', 2048);
 ini_set('xdebug.var_display_max_data', 102400);
 ini_set('session.gc_maxlifetime', 86400);
-
+echo "jajsjad";	
 
 ?>
 
---
+++ tabla.php
@@ -0,0 +1,32 @@
+<div class="row">
+	<div class="col-sm-12">
+		<h2> Tabla figuritas Copa America 2021</h2>
+		<table class="table table-hover table-condensed table-bordered">
+			<tr>
+				<td><div class=" col text-center"><button class = "btn btn-danger">1</button></div></td>
+				<td><div class=" col text-center"><button class = "btn btn-danger">2</button></div></td>
+				<td><div class=" col text-center"><button class = "btn btn-danger">3</button></div></td>
+				<td><div class=" col text-center"><button class = "btn btn-danger">4</button></div></td>
+				<td><div class=" col text-center"><button class = "btn btn-danger">5</button></div></td>
+				<td><div class=" col text-center"><button class = "btn btn-danger">6</button></div></td>
+				<td><div class=" col text-center"><button class = "btn btn-danger">7</button></div></td>
+				<td><div class=" col text-center"><button class = "btn btn-danger">8</button></div></td>
+				<td><div class=" col text-center"><button class = "btn btn-danger">9</button></div></td>
+				<td><div class=" col text-center"><button class = "btn btn-danger">10</button></div></td>
+	
+			</tr>
+			<tr>
+				<td><button class = "btn btn-danger">11</button></td>
+				<td><button class = "btn btn-danger">12</button></td>
+				<td><button class = "btn btn-danger">13</button></td>
+				<td><button class = "btn btn-danger">14</button></td>
+				<td><button class = "btn btn-danger">15</button></td>
+				<td><button class = "btn btn-danger">16</button></td>
+				<td><button class = "btn btn-danger">17</button></td>
+				<td><button class = "btn btn-danger">18</button></td>
+				<td><button class = "btn btn-danger">19</button></td>
+				<td><button class = "btn btn-danger">20</button></td>
+			</tr>
+		</table>
+	</div>
+</div>
