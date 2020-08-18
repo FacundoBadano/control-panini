d11a74afa347f7208c3a85626a8d97a5c0cd6a57
Facundo Badano <facundobadano6195@gmail.com>
2020-08-18 10:40:51 -0300

    htacces

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
 <!DOCTYPE html>
+<html>
+<head>
+	<title>Tabla Figuritas</title>
+</head>
+<body>
+
+</body>
+</html>
 
 <?php
-phpinfo();
+//phpinfo();
 ini_set('xdebug.var_display_max_depth', 10);
 ini_set('xdebug.var_display_max_children', 2048);
 ini_set('xdebug.var_display_max_data', 102400);
 ini_set('session.gc_maxlifetime', 86400);
 
-<br>
-<hr />
-<h2>Tabla control</h2>
-<hr />
 
-
-<div class="row">
-	<div class="col-sm-12">
-		<table class="table table-hover table-condensed table-bordered">
-			<tr>
-
-			</tr>
-		</table>	
-	</div>
-</div>
-
+?>
 
 
 
